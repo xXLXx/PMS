6a2cc7f743da3b882e840f12e4f736833f1112d1
Ellen <ellen051394@gmail.com>
2015-01-05 13:05:46 +0800

    Add Facebook connect

--- src/com/qrc/pms/FindPeopleFragment.java
+++ src/com/qrc/pms/FindPeopleFragment.java
@@ -3,37 +3,42 @@ package com.qrc.pms;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Currency;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.sax.RootElement;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.util.Log;	
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 
 import com.actionbarsherlock.app.SherlockFragment;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.facebook.Session;
+
+import com.facebook.FacebookException;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.OnErrorListener;
 
 //edited
 public class FindPeopleFragment extends SherlockFragment {
 //	private GalleryViewPager mViewPager;
 	
+	private static final String TAG = null;
 	public static Button connect_to_facebook;
 	public View rootView;
 	
-	public FindPeopleFragment(){
-		
-	}
+	public FindPeopleFragment(){}
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
+		
          rootView = inflater.inflate(R.layout.fragment_find_people, container, false);
         return rootView;
     }
@@ -43,59 +48,23 @@ public class FindPeopleFragment extends SherlockFragment {
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
 	   
-		connect_to_facebook = (Button) view.findViewById(R.id.btn_connect_fb);
-		
-		connect_to_facebook.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-				
-				
-			}
-		});
-			
-//        CustomPagerAdapter adapter = new CustomPagerAdapter(new int[] {R.drawable.antismoke, R.drawable.antismoke1, R.drawable.antismoke2, R.drawable.antismoke3});
-//        ViewPager myPager = (ViewPager) getActivity().findViewById(R.id.customviewpager);
-//        myPager.setAdapter(adapter);
-//        myPager.setCurrentItem(0);
-		
-//		String[] urls = null;
-//        List<String> items = new ArrayList<String>();
-//		try {
-//			urls = getActivity().getAssets().list("");
-//	
-//	        for (String filename : urls) 
-//	        {
-//	        	if (filename.matches(".+\\.people.jpg")) 
-//	        	{
-//	        		String path = getActivity().getFilesDir() + "/" + filename;
-//	        		copy(getActivity().getAssets().open(filename), new File(path) );
-//	        		items.add(path);
-//	        	}
-//	        }
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+//		connect_to_facebook = (LoginButton) view.findViewById(R.id.authButton);
 //		
-//		FilePagerAdapter pagerAdapter = new FilePagerAdapter(getActivity(), items);
-//		mViewPager = (GalleryViewPager) getActivity().findViewById(R.id.viewer);
-//        mViewPager.setOffscreenPageLimit(3);
-//        mViewPager.setAdapter(pagerAdapter);
+//		
+//		connect_to_facebook.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View arg0) {
+//				// TODO Auto-generated method stub
+//				
+//
+//			}
+//		});
+
 	}
 	
-	public void copy(InputStream in, File dst) throws IOException {
 
-//        OutputStream out = new FileOutputStream(dst);
-//
-//        // Transfer bytes from in to out
-//        byte[] buf = new byte[1024];
-//        int len;
-//        while ((len = in.read(buf)) > 0) {
-//            out.write(buf, 0, len);
-//        }
-//        in.close();
-//        out.close();
-    }
+
+
+	
 }
